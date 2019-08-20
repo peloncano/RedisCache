20c0c9c77f3baf7504f617997caed4eeaca598ed
Luis Cano <luis.cano@ogilvy.com>
2019-08-20 11:46:02 -0400

    Moved some methods from Cake's RedisEngine (groups(), clearGroup)

--- Lib/Cache/Engine/RedisEngine.php
+++ Lib/Cache/Engine/RedisEngine.php
@@ -3,7 +3,8 @@
 /**
  * Redis Cache Class. Uses Predis as the PHP connection class
  */
-class RedisEngine extends CacheEngine {
+class RedisEngine extends CacheEngine
+{
 
 
     /**
@@ -13,6 +14,12 @@ class RedisEngine extends CacheEngine {
      */
     public $settings = array();
 
+    /**
+     * Predis wrapper.
+     *
+     * @var _Predis
+     */
+    protected $_Predis = null;
 
     /**
      * Initialize the Redis Cache Engine
@@ -23,37 +30,53 @@ class RedisEngine extends CacheEngine {
      * @param array $settings array of setting for the engine
      * @return boolean true
      */
-    public function init( $settings = array() ) {
+    public function init($settings = array())
+    {
 
-        parent::init(array_merge(array(
-            'engine'=> 'Redis'
-            , 'prefix' => Inflector::slug(APP_DIR) . '_'
+        if (!class_exists('Predis\Client')) {
+            return false;
+        }
+
+        parent::init(
+            array_merge(array(
+                'engine' => 'Redis', 'prefix' => Inflector::slug(APP_DIR) . '_'
             ), $settings)
         );
 
-        if ( ! isset($this->_Predis)) {
-            $this->_Predis = new Predis\Client(
-                RedisConfig::$default['cache']
-                , array('prefix' => $this->settings['prefix'])
-            );
+        // if ( ! isset($this->_Predis)) {
+        //     $this->_Predis = new Predis\Client(
+        //         RedisConfig::cacheConfig()
+        //         , array('prefix' => $this->settings['prefix'])
+        //     );
+        // }
+
+
+
+        return $this->_connect();
+    }
+
+    /**
+     * Connects to a Redis server
+     *
+     * @return bool True if Redis server was connected
+     */
+    protected function _connect()
+    {
+        try {
+
+            if (!isset($this->_Predis)) {
+                $this->_Predis = new Predis\Client(
+                    RedisConfig::cacheConfig(),
+                    array('prefix' => RedisConfig::$globalCachePrefix)
+                );
+            }
+
+        } catch (Exception $e) {
+            return false;
         }
 
         return true;
     }
-
-
-    /**
-     * READ
-     * Return whatever is stored in the key
-     *
-     * @param string $key
-     * @access public
-     * @return boolean
-     */
-    public function read( $key = '' ) {
-        return $this->_expand($this->_Predis->get($key));
-    }
-
 
     /**
      * WRITE
@@ -65,10 +88,41 @@ class RedisEngine extends CacheEngine {
      * @access public
      * @return boolean
      */
-    public function write( $key = '', $data = null, $duration = 3600 ) {
-        return $this->_Predis->setex($key, $duration, $this->_compress($data));
+    public function write($key = '', $data = null, $duration = 3600)
+    {
+
+        $data = $this->_compress($data);
+
+        if (!$this->_Predis->isConnected()) {
+            $this->_connect();
+        }
+
+        if ($duration === 0) {
+            return $this->_Predis->set($key, $data);
+        }
+
+        return $this->_Predis->setex($key, $duration, $data);
     }
 
+    /**
+     * READ
+     * Return whatever is stored in the key
+     *
+     * @param string $key
+     * @access public
+     * @return boolean
+     */
+    public function read($key = '')
+    {
+
+        $value = $this->_Predis->get($key);
+
+        if(is_null($value)) {
+            return false;
+        }
+
+        return $this->_expand($value);
+    }
 
     /**
      * INCREMENT
@@ -79,8 +133,9 @@ class RedisEngine extends CacheEngine {
      * @access public
      * @return boolean
      */
-    public function increment( $key = '', $offset = 1 ) {
-        return $this->_Predis->incrby($key, $offset);
+    public function increment($key = '', $offset = 1)
+    {
+        return (int) $this->_Predis->incrby($key, $offset);
     }
 
 
@@ -93,8 +148,9 @@ class RedisEngine extends CacheEngine {
      * @access public
      * @return boolean
      */
-    public function decrement( $key = '', $offset = 1 ) {
-        return $this->_Predis->decrby($key, $offset);
+    public function decrement($key = '', $offset = 1)
+    {
+        return (int) $this->_Predis->decrby($key, $offset);
     }
 
 
@@ -106,24 +162,104 @@ class RedisEngine extends CacheEngine {
      * @access public
      * @return boolean
      */
-    public function delete( $key = '' ) {
+    public function delete($key = '')
+    {
         // Predis::del returns an integer 1 on delete, convert to boolean
-        return $this->_Predis->del($key) ? true : false;
+        return $this->_Predis->del($key) > 0;
     }
 
 
     /**
-     * CLEAR - not implemented
      * DEL all keys with settings[prefix]
      *
      * @param boolean $check
      * @access public
      * @return boolean
      */
-    public function clear( $check ) {
+    public function clear($check)
+    {
+        if ($check) {
+            return true;
+        }
+
+        error_log(print_r($this->settings['prefix'] . '*', true)); //TODO delete me
+        $keys = $this->_Predis->keys($this->settings['prefix'] . '*');
+        error_log(print_r($keys, true)); //TODO delete me
+        if(!empty($keys)) {
+            $this->_Predis->del($keys);
+        }
+
         return true;
     }
 
+    /**
+     * Returns the `group value` for each of the configured groups
+     * If the group initial value was not found, then it initializes
+     * the group accordingly.
+     *
+     * @return array
+     */
+    public function groups()
+    {
+        $result = array();
+        foreach ($this->settings['groups'] as $group) {
+            $value = $this->_Predis->get($this->settings['prefix'] . $group);
+            if (!$value) {
+                $value = 1;
+                $this->_Predis->set($this->settings['prefix'] . $group, $value);
+            }
+            $result[] = $group . $value;
+        }
+        return $result;
+    }
+
+    /**
+     * Increments the group value to simulate deletion of all keys under a group
+     * old values will remain in storage until they expire.
+     *
+     * @param string $group The group name to clear.
+     * @return bool success
+     */
+    public function clearGroup($group)
+    {
+        error_log(print_r($this->settings['prefix'] . $group, true)); //TODO delete me
+        return (bool) $this->_Predis->incr($this->settings['prefix'] . $group);
+    }
+
+
+    /**
+     * Disconnects from the redis server
+     */
+    public function __destruct()
+    {
+        if (empty($this->settings['persistent']) && $this->_Predis !== null) {
+            $this->_Predis->disconnect();
+        }
+    }
+
+    /**
+     * Write data for key into cache if it doesn't exist already.
+     * If it already exists, it fails and returns false.
+     *
+     * @param string $key Identifier for the data.
+     * @param mixed $value Data to be cached.
+     * @param int $duration How long to cache the data, in seconds.
+     * @return bool True if the data was successfully cached, false on failure.
+     */
+    public function add($key, $value, $duration)
+    {
+        return true;
+        if (!is_int($value)) {
+            $value = serialize($value);
+        }
+
+        $result = $this->_Predis->setnx($key, $value);
+        // setnx() doesn't have an expiry option, so overwrite the key with one
+        if ($result) {
+            return $this->_Predis->setex($key, $duration, $value);
+        }
+        return false;
+    }
 
     /**
      * GARBAGE COLLECTION - not required
@@ -131,7 +267,8 @@ class RedisEngine extends CacheEngine {
      * @access public
      * @return boolean
      */
-    public function gc($expires = null) {
+    public function gc($expires = null)
+    {
         return true;
     }
 
@@ -144,8 +281,9 @@ class RedisEngine extends CacheEngine {
      * @return null|string
      * @access private
      */
-    private function _compress( $data = null ) {
-        if(is_array($data) || is_object($data) || ! preg_match('/^\d$/', $data)) {
+    private function _compress($data = null)
+    {
+        if (is_array($data) || is_object($data) || !preg_match('/^\d$/', $data)) {
             $return = serialize($data);
         } else {
             $return = $data;
@@ -162,8 +300,9 @@ class RedisEngine extends CacheEngine {
      * @return mixed
      * @access private
      */
-    private function _expand( $data = null ) {
-        if(false === $return = @unserialize($data)) {
+    private function _expand($data = null)
+    {
+        if (false === $return = @unserialize($data)) {
             $return = $data;
         }
 
