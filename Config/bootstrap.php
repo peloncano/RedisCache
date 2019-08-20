a1d3a0a8000ea77ad54111ef9aaa88d68ce00165
Luis Cano <luis.cano@ogilvy.com>
2019-08-20 11:44:52 -0400

    bootstrapped with default configuration

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -7,18 +7,46 @@
  */
 class RedisConfig {
 
-    static public $default = array(
-        'cache' => array(
-            'hostname' => '127.0.0.1'
-            , 'port' => 6379
-            , 'password' => null
-        )
-        , 'session' => array(
-            'hostname' => '127.0.0.1'
-            , 'port' => 6379
-            , 'password' => null
-        )
-    );
+    static public $default = [
+            'scheme' => 'tcp',
+            'host' => '127.0.0.1',
+            'port' => 6379,
+            'password' =>  null
+        ];
+
+    /**
+     * String that will be added as a prefix in the redis cache client
+     *
+     * @var string
+     */
+    static public $globalCachePrefix = '';
+    
+    /**
+     * String that will be added as a prefix in the redis session client
+     *
+     * @var string
+     */
+    static public $globalSessionPrefix = '';
+
+    public static function cacheConfig() {
+
+        $customConfiguration = (array)Configure::read('RedisCache.cache');
+
+        return array_merge(
+            self::$default,
+            $customConfiguration
+        );
+    }
+
+    public static function sessionConfig() {
+
+        $customConfiguration = (array) Configure::read('RedisCache.session');
+
+        return array_merge(
+            self::$default,
+            $customConfiguration
+        );
+    }
 }
 
 // Redis plugin depends on Predis
