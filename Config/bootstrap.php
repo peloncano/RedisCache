b8b11ca9fee50e51b4f5f45810b79681fce95d2c
Luis Cano <luis.cano@ogilvy.com>
2019-08-19 21:35:46 -0400

    import predis from composer install

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -23,5 +23,5 @@ class RedisConfig {
 
 // Redis plugin depends on Predis
 // @link https://github.com/nrk/predis
-App::import('Lib', 'RedisCache.Predis/Autoloader');
-Predis\Autoloader::register();
+App::import('Vendor', 'predis/predis/autoload');
+// Predis\Autoloader::register();/
